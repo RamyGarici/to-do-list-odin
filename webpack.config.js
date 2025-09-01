5492ebdfaa01a9677310061f60496e40e27d0583
RamyGarici <ramymanilgarici@gmail.com>
2025-09-01 12:44:04 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js', 
+  output: {
+    filename: 'bundle.js', 
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, 
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html', 
+    }),
+  ],
+  devServer: {
+    static: './dist', 
+    port: 3000,       
+    open: true,       
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,         
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/i,  
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i, 
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
